03006d35741db6b1839c13f17198dff644c52f18
Simon Smale <simon@smalemail.net>
2023-11-25 21:28:20 +0000

    feat: adds conditional on property

--- facades/src/main/java/uk/co/iamsimonsmale/facades/DynmicString/autoconfigure/DynamicStringMakerBeanDefinitionRegistrarConfiguration.java
+++ facades/src/main/java/uk/co/iamsimonsmale/facades/DynmicString/autoconfigure/DynamicStringMakerBeanDefinitionRegistrarConfiguration.java
@@ -1,5 +1,6 @@
 package uk.co.iamsimonsmale.facades.DynmicString.autoconfigure;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -7,6 +8,7 @@ import org.springframework.core.env.Environment;
 @Configuration
 public class DynamicStringMakerBeanDefinitionRegistrarConfiguration {
   @Bean
+  @ConditionalOnProperty("uk.co.iamsimonsmale.dynamic-strings.lastName")
   public static DynamicStringMakerBeanDefinitionRegistrar beanDefinitionRegistrar(Environment environment) {
     return new DynamicStringMakerBeanDefinitionRegistrar(environment);
   }
